cb299db8ee9cd0e4cf74b45a617aa33956ebcdb8
AlexanderInsulin <insulin@protonmail.com>
2018-02-09 16:16:32 +0300

    Write test for editing and deleting album action.

--- src/actions/albumActions.test.js
+++ src/actions/albumActions.test.js
@@ -2,11 +2,27 @@ import * as actions from './albumActions';
 
 describe('album actions', () => {
 
-  it('should add album', () => {
-    let name = '';
+  it('should create add album action', () => {
+    let name = 'test name';
     expect(actions.addAlbum(name)).toEqual({
+      type: actions.ADD_ALBUM,
       name: name,
-      photos: []
+    });
+  });
+
+  it('should create edit album action', () => {
+    let newName = 'new name';
+    expect(actions.editAlbum(uid , newName)).toEqual({
+      type: actions.EDIT_ALBUM,
+      uid: uid,
+      newName: newName,
+    });
+  });
+
+  it('should create delete album action', () => {
+    expect(actions.editAlbum(uid)).toEqual({
+      type: actions.DELETE_ALBUM,
+      uid: uid,
     });
   });
 });
