2356f65c877aafea55860eac103fdd65c50feb51
AlexanderInsulin <insulin@protonmail.com>
2018-02-09 22:26:46 +0300

    Done photo creation.

--- src/reducers/photo.js
+++ src/reducers/photo.js
@@ -1,3 +1,6 @@
+import actions from '../actions/';
+import {v4} from 'uuid';
+
 const initialState = {
   uuid: 0,
   name: '',
@@ -6,6 +9,12 @@ const initialState = {
 
 const photo = (state = initialState, action) => {
   switch (action.type) {
+    case actions.photo.ADD_PHOTO:
+      return {
+        uuid: v4(),
+        name: action.name,
+        imageURL: action.imageURL
+      }
     default:
       return state;
   }
