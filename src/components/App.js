6222efb6fdb50051429d282929c2c534a6654215
zyec32 <njkmrjpfzw@mail.ru>
2018-02-09 16:34:39 +0300

    add galery

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
-import AlbumCreate from './AlbumCreate';
-import Album from './Album';
-import { Container, Row } from 'reactstrap'
+import Galery from './Galery';
 
 class App extends Component {
   render() {
     return (
-      <Container>
-        <Row>
-          <AlbumCreate />
-          <Album name={'cock'}/>
-          <Album name={'vehdk'} image={'https://pp.userapi.com/c636925/v636925368/461ac/We0oi2Hg_dU.jpg'}/>
-          <Album name={'caojiec'}/>
-        </Row>
-      </Container>
+      <Galery />
     );
   }
 }
---
+++ src/components/Galery.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import AlbumCreate from './AlbumCreate';
+import Album from './Album';
+import { Container, Row } from 'reactstrap'
+
+class Galery extends Component {
+	render() {
+		return (
+			<Container>
+				<Row>
+					<AlbumCreate />
+					<Album name={'cock'} />
+					<Album name={'vehdk'} image={'https://pp.userapi.com/c636925/v636925368/461ac/We0oi2Hg_dU.jpg'} />
+					<Album name={'caojiec'} />
+				</Row>
+			</Container>
+		);
+	}
+}
+
+export default Galery;
