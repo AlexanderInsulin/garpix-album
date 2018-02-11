ae2d7949dd0a45c197b767f93f393753f8346650
AlexanderInsulin <insulin@protonmail.com>
2018-02-11 05:54:39 +0300

    Connect redux to react app.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import galeryApp from './reducers';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(galeryApp);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import album from './album';
+
+const galeryApp = combineReducers({
+  album
+})
+
+export default galeryApp;
