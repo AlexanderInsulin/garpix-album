5e11e7545dcb801cd1d51903842e582e7576d044
AlexanderInsulin <insulin@protonmail.com>
2018-02-11 08:36:16 +0300

    Refactor index for galery.

--- src/index.js
+++ src/index.js
@@ -7,9 +7,14 @@ import registerServiceWorker from './registerServiceWorker';
 import galeryApp from './reducers';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'
+import actions from './actions/'
 
 
-const store = createStore(galeryApp);
+const store = createStore(
+  galeryApp,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+store.dispatch(actions.album.addAlbum('s'));
 
 ReactDOM.render(
   <Provider store={store}>
